affe756f48e5a634a34f1842cabbb7222ae2dad4
Ernest Beecherl <ernestbeecherl98@gmail.com>
2024-01-30 14:16:50 -0600

    pushing code

--- index.js
+++ index.js
@@ -1,74 +1,76 @@
-//GIVEN a command-line application that accepts user input
-const inquirer = require('inquirer'); 
-const fs = require('fs'); 
-const {Circle, Square, Triangle } = require('./lib/shapes.js'); 
-const {writeFIle} = require('fs/promises'); 
-const SVG = require('./lib/shapes.test.js'); 
+// constants to pull in required inquirer module and shape + svg objects
+const  inquirer = require("inquirer");
+const fs = require('fs');
+const {Circle, Triangle, Square} = require("./lib/shapes.js");
+const {writeFile} = require("fs/promises");
+const SVG = require("./lib/svg.js");
 
+
+// function uses inquirer to ask user for desired logo characteristics
 function createLogo() {
-    inquirer 
-        .prompt([
-        {
-            type: "input",
-            name: "logoChars",
-            message: "Enter up to three characters for your logo.",
-            validate: charsInput => {
-                if (charsInput) {
-                    return true; 
-                } else {
-                    console.log("Enter between 1 and 3 charactors")
-                    return false;
-                }
-            },
-        },
-        {
-            type: 'input',
-            name: 'textColor',
-            message: 'Enter a color or hexidecimal number for your text',
-        },
-        {
-            type: 'list',
-            name: 'shape',
-            message: 'Plase select a shape.',
-            choices: ["Circle", "Square", "Triangle"],
-
-        },
-        {
-            type: 'input',
-            name: 'shapeColor',
-            message: 'Enter a color or hexidecimal number for your text',
-        },
-        ])
-
-        //enter imputs for all prompts
-        .then(({logoChars, textColor, shape, shapeColor}) => {
-            let shapetype;
-            switch (shape) {
-                case 'triangle':
-                    shapeType = new Triangle()
-                    break;
-
-                case 'square':
-                    shapeType = new Square()
-                    break;
-                
-                default:
-                case 'circle':
-                    shapeType = new Circle()
-                    break; 
+inquirer
+    .prompt([
+    {
+        type: "input",
+        name: "charsLogo",
+        message: "Please, enter up to 3 characters for your logo",
+        validate: charsInput => {
+            if (charsInput) {
+                return true;
+            } else {
+                console.log("Please, enter between 1 and 3 characters")
+                return false;
             }
-            // generates new logo with the specific characteristics 
-            shapeType.setColor(shapeColor);
-            const svg = new SVG();
-            svg.setTextColor(textColor, logoChars); 
-            svg.setShape(shape); 
-            return writeFile('logo.svg', svg.render())
-        }).then(() => {
-            console.log('Generate logo.svg'); 
-        }).catch((error)=> {
-            console.log(error);
-        })
+        }
+    },
+    {
+        type: "input",
+        name: "charsColor",
+        message: "Enter a color for characters",
+    },
+    {
+        type: "list",
+        name: "shapeType",
+        message: "Pick one of the following shapes for your logo",
+        choices: ["Circle", "Triangle", "Square"]
+    },
+    {
+        type: "input",
+        name: "shapeColor",
+        message: "Enter a color to give the shape more style",
+    },
 
+    ])
+
+    // use logo characteristics to generate svg file
+    .then (({ charsLogo, charsColor, shapeType, shapeColor}) => {
+    var shape;
+    switch (shapeType) {
+        case "Circle":
+        shape = new Circle();
+        break;
+        case "Triangle":
+        shape = new Triangle();
+        break;
+        case "Square":
+        shape = new Square();
+        break;
+    }
+    // generates the new logo
+    shape.setColor(shapeColor);
+    const svg = new SVG();
+    svg.setText(charsLogo, charsColor);
+    svg.setShape(shape)
+    const svgMarkup = svg.render();
+    return writeFile("logo.svg", svgMarkup);
+    })
+    .then(() => {
+    console.log("Successfully created logo! Open in live server to see your finished product.")
+    })
+    .catch((error) => {
+    console.log("Error!", error)
+    });
 }
 
-init(); 
+// call createLogo function
+createLogo();
